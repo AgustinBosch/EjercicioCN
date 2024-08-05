e393795626ea395bb1a3992afc18ad09ceb30f51
Agustin <agustinbosch@gmail.com>
2024-08-05 09:29:10 -0300

    Tasklist wip

--- to-do-list-frontend/src/routes/ToDoList/Task.svelte
+++ to-do-list-frontend/src/routes/ToDoList/Task.svelte
@@ -1,1 +1,65 @@
-<h2>this is a task</h2>
+<script>
+  export let id;
+  export let title;
+  export let description;
+  export let isComplete;
+  export let color;
+
+  const colorComplete = "#9fcf44";
+  const colorIncomplete = "#dd8430";
+
+  const toggleTask = () => {
+    isComplete = !isComplete;
+    console.log(isComplete);
+  };
+</script>
+
+<div
+  class="task"
+  style="border-color:{color}"
+  on:click={toggleTask}
+  on:keydown={toggleTask}
+  role="button"
+  tabindex="0"
+>
+  <div
+    class="triangle"
+    style="--color:{isComplete ? colorComplete : colorIncomplete}"
+  ></div>
+  <h3>{title}</h3>
+  <span>{description}</span>
+</div>
+
+<style>
+  .triangle {
+    position: absolute;
+    top: -0.5px;
+    right: -0.5px;
+    width: 0;
+    height: 0;
+    border-left: 25px solid transparent;
+    border-top: 25px solid var(--color);
+    border-top-right-radius: 7px;
+  }
+  .task {
+    position: relative;
+    padding: 5px 15px;
+    border: 5px solid;
+    border-radius: 10px;
+    text-align: center;
+    min-width: 120px;
+    height: fit-content;
+    background-color: white;
+    transition: padding 0.5s ease;
+    cursor: pointer;
+  }
+
+  .task:hover {
+    padding: 15px 25px;
+  }
+
+  h3 {
+    margin-bottom: 5px;
+    margin-top: 0;
+  }
+</style>
--- to-do-list-frontend/src/routes/ToDoList/ToDoList.svelte
+++ to-do-list-frontend/src/routes/ToDoList/ToDoList.svelte
@@ -3,18 +3,69 @@
   import Task from "./Task.svelte";
   import taskService from "./taskService";
 
+  const colors = [
+    "#a6c7ea",
+    "#89bbde",
+    "#6899aa",
+    "#a9dbd2",
+    "#8fcfb4",
+    "#88bbaa",
+    "#bbdcad",
+    "#bbcab3",
+    "#f9f18c",
+    "#dfe88d",
+    "#cbac8e",
+    "#fbdddd",
+    "#f8b2bd",
+    "#f59597",
+    "#f9a485",
+    "#efa695",
+    "#fdbd7d",
+    "#ceb3d6",
+    "#b3b3d9",
+    "#c0c0c0",
+  ];
+
+  let allTasks = [];
+
   onMount(async () => {
     cargarInformacion();
   });
 
   const cargarInformacion = async () => {
-    let all = await taskService.getAll();
-    console.log(all);
+    allTasks = await taskService.getAll();
+    console.log(allTasks);
+  };
+
+  const getRandomColor = () => {
+    let index = Math.floor(Math.random() * (colors.length - 1));
+    return colors[index];
   };
 </script>
 
-<h1>Esta es una lista de tasks</h1>
-
-<div>
-  <Task />
+<div class="box">
+  <h2 class="fancy" style="border-color:{getRandomColor()}">
+    Esta es una lista de tasks
+  </h2>
 </div>
+
+<div class="box">
+  {#each allTasks as task}
+    <Task {...task} color={getRandomColor()} />
+  {/each}
+</div>
+
+<style>
+  .box {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    flex-wrap: wrap;
+  }
+  .fancy {
+    padding: 5px 20px;
+    background-color: white;
+    border: 3px solid;
+    border-radius: 10px;
+  }
+</style>
